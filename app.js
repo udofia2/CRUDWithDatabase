bbf8fffce49adc446708f8c7449f3d11bd5d674e
Abasiodiong Udofia <enalsy22@gmail.com>
2021-05-11 09:47:23 +0100

    server connected

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const express = require('express')
 
 const app = express()
 
-
-
 app.use((req, res, next) => {
   res.status(404).send(`Sorry, can't find ${req.url}`)
+  next()
 })
 
 app.listen(parseInt(process.env.PORT), () => console.log(`Server is connected on ${parseInt(process.env.PORT)}`))
