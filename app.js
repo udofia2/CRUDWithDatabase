ec61d3500b7427701b93caa7a9cba2a06546a3de
Abasiodiong Udofia <enalsy22@gmail.com>
2021-05-11 09:36:32 +0100

    app.js setup

---
+++ app.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+
+
+app.use((req, res, next) => {
+  res.status(404).send(`Sorry, can't find ${req.url}`)
+})
+
+app.listen(parseInt(process.env.PORT), () => console.log(`Server is connected on ${parseInt(process.env.PORT)}`))
+
+module.exports = app
